8310148b0233f6e10e3e806ac1f491ce7c6774d9
TimZ <tiomsity@gmail.com>
2022-09-16 22:21:51 -0500

    added timer picker styles

--- my-app/src/components/modal/modal_contents/TimePicker.js
+++ my-app/src/components/modal/modal_contents/TimePicker.js
@@ -1,13 +1,33 @@
 import React from "react";
-// import styles from "./TimePicker.module.css";
+import styles from "./TimePicker.module.css";
 import ModalModeForm from "../../reusables/ModalModeForm";
 
 const TimePicker = ({ closeModal }) => {
   return (
-    <ModalModeForm
-      title={"Pick a timer"}
-      closeModal={closeModal}
-    ></ModalModeForm>
+    <ModalModeForm title={"Set a timer"} closeModal={closeModal}>
+      <div className={styles["timer-inputs"]}>
+        <div className={styles["input"]}>
+          <input
+            type="number"
+            id="min-input"
+            name="min-input"
+            min="1"
+            max="59"
+          ></input>
+          <label for="min-input">Min</label>
+        </div>
+        <div className={styles["input"]}>
+          <input
+            type="number"
+            id="sec-input"
+            name="sec-input"
+            min="0"
+            max="59"
+          ></input>
+          <label for="sec-input">Sec</label>
+        </div>
+      </div>
+    </ModalModeForm>
   );
 };
 
