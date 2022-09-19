54ad24fc3ed1d2686fd8ff9aa7ba16cee8951862
TimZ <tiomsity@gmail.com>
2022-09-18 22:53:04 -0500

    deleted Timer.module.css and Race.module.css

--- my-app/src/components/game_mode/Race.js
+++ my-app/src/components/game_mode/Race.js
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from "./Race.module.css";
 
 const Race = ({ race }) => {
   return (
-    <div className={styles["finish-line"]}>
+    <>
       <span>{race}</span>
-    </div>
+    </>
   );
 };
 
--- my-app/src/components/game_mode/Timer.js
+++ my-app/src/components/game_mode/Timer.js
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "./Timer.module.css";
 
 const Timer = ({ timeString }) => {
   return (
-    <div className={styles.timer}>
-      <span className={styles.minutes}>{timeString}</span>
+    <div>
+      <span>{timeString}</span>
     </div>
   );
 };
