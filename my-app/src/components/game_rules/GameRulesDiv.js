03d07970ef33aa4c6668cb4426c354a51dcfe61f
TimZ <tiomsity@gmail.com>
2022-09-28 09:47:47 -0500

    added game rules panel

--- my-app/src/components/game_rules/GameRulesDiv.js
+++ my-app/src/components/game_rules/GameRulesDiv.js
@@ -1,15 +1,36 @@
-import React from "react";
+import { useState } from "react";
 import Button from "../reusables/Button";
 import SidePanelDiv from "../reusables/SidePanelDiv";
 import SidePanelWrapper from "../reusables/SidePanelWrapper";
 
 const GameRulesDiv = () => {
+  const [rulesPanelOpen, setRulesPanelOpen] = useState(false);
+
+  const openRulesPanel = () => {
+    setRulesPanelOpen(true);
+  };
+
+  const closeRulesPanel = () => {
+    setRulesPanelOpen(false);
+  };
+
   return (
     <SidePanelDiv location={{ left: "0" }}>
       <Button
         buttonContent="❓"
         extraStyles={{ width: "4.2rem", top: "2rem", left: "20%" }}
+        onClick={openRulesPanel}
       />
+
+      <SidePanelWrapper
+        panelDefaultPosition={{ transform: "translateX(-100%)" }}
+        sidePanelShown={rulesPanelOpen}
+        controlBtnContent="⏪"
+        controlBtnHorizontalLocation={{ left: "20%" }}
+        handleSidePanelBtn={closeRulesPanel}
+      >
+        "All the rules go here"
+      </SidePanelWrapper>
     </SidePanelDiv>
   );
 };
